581b857f320625b347391f0680fb021a6963c230
Silvio Ramalho <silvioramalho@gmail.com>
2020-05-16 18:56:26 -0300

    Adding style to routes index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import {View, ActivityIndicator} from 'react-native';
+import {View, ActivityIndicator, StyleSheet} from 'react-native';
 
 import {useAuth} from '../contexts/auth.context';
 
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
 const Routes: React.FC = () => {
   const {signed, loading} = useAuth();
 
   if (loading) {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={styles.container}>
         <ActivityIndicator size="large" color="#666" />
       </View>
     );
